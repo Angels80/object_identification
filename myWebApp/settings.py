3efa13ce0261e4bc51c270a519a64886a80d22e1
Angels80 <74528045+Angels80@users.noreply.github.com>
2021-06-14 14:37:49 +0530

    Update settings.py

--- myWebApp/settings.py
+++ myWebApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pev@6rswb15nqfk(#2v-)opn3xxtd6fd)-g0)en%u+c!u!psaz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','object-identification-80.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1','object-identification-80.herokuapp.com']
 
 
 # Application definition
